d77a939a52eddd14813e6613759b0cc6a325c21c
veronikaKochugova <veronika.kochugova@gmail.com>
2019-04-13 19:19:05 +0300

    save

--- lab3/main.py
+++ lab3/main.py
@@ -3,60 +3,59 @@ import numpy as np
 
 def print_matrix(name, shape, values):
     print("Matrix {name}{shape} :\n{values}\n".format(name=name, shape=shape, values=values))
-    # np.savetxt("{name}.csv".format(name=name), values, delimiter=",")
+    np.savetxt("{name}.csv".format(name=name), values, delimiter=",")
 
 
 def method1(y, filter):
-    x_row = x.shape[0]
-    x_col = x.shape[1]
-    h_row = h.shape[0]
-    h_col = h.shape[1]
-    y_row = (h_row + x_row - 1)
-    y_col = (h_col + x_row - 1)
-    y = np.zeros((y_row, y_col))
-    for i in range(y_row):
-        for j in range(y_col):
-            for k1 in range(h_row):
-                for k2 in range(h_col):
-                    if (i - k1 >= 0) & (i - k1 < x_row) & (j - k2 < x_col) & (j - k2 >= 0):
-                        y[i, j] += h[k1, k2] * x[i - k1, j - k2]
+    row = y.shape[0]
+    col = y.shape[1]
+    window = 3
+    signal = y
+    for i in range(1, row - 1):
+        for j in range(1, col - 1):
+            sg = 0
+            for k1 in range(window):
+                for k2 in range(window):
+                    sg += filter[k1, k2] * y[i - 1 + k1, j - 1 + k2]
+            signal[i, j] = sg
     return y
 
 
-def method2(h, x):
-    x_row = x.shape[0]
-    x_col = x.shape[1]
-    h_row = h.shape[0]
-    h_col = h.shape[1]
-    y_row = (h_row + x_row - 1)
-    y_col = (h_col + x_row - 1)
-    y = np.zeros((y_row, y_col))
-    count = 0
-    for k1 in range(x_row):
-        for k2 in range(x_col):
-            temp = np.zeros((y_row, y_col))
-            for m1 in range(h_row):
-                for m2 in range(h_col):
-                    temp[m1 + k1, m2 + k2] = h[m1, m2] * x[k1, k2]
-            count += 1
-            print_matrix("TEMP" + str(count), temp.shape, temp)
-            y += temp
+def method2(y, m):  # statistic filter(nu=m * sigma)
+    row = y.shape[0]
+    col = y.shape[1]
+    window = 3
+    signal = y
+    for i in range(1, row - 1):
+        for j in range(1, col - 1):
+            sum1 = 0
+            sum2 = 0
+            for k1 in range(window):
+                for k2 in range(window):
+                    sum1 += y[i - 1 + k1, j - 1 + k2]
+            G = sum1 / pow(window, 2)
+            for k1 in range(window):
+                for k2 in range(window):
+                    sum2 += pow(y[i - 1 + k1, j - 1 + k2] - G, 2)
+            D = sum2 / (pow(window, 2) - 1)
+            nu = m * pow(D, 0.5)
+            signal[i, j] = y[i, j] if (y[i, j] - G) < nu else G
     return y
 
 
 if __name__ == '__main__':
-
     # read result matrix from lab 2
     y = np.genfromtxt('../lab2/Y.csv', delimiter=',')
     print_matrix("Y", y.shape, y)
     # add noise
-    y[6,5] = 400
-    y[8,9] = 400
+    y[6, 5] = 400
+    y[8, 9] = 400
     print_matrix("Y with noise", y.shape, y)
-    filter = np.matrix([[3,2,1],[2,4,2],[1,2,3]])
+    filter = (1 / 19) * np.matrix([[3, 2, 1], [2, 4, 2], [1, 2, 3]])
+    print_matrix("Filter", filter.shape, filter)
     print("=============================== Method 1 ===============================")
-    y = method1(y, filter)
-    # print_matrix("Y", y.shape, y)
+    y1 = method1(y, filter)
+    print_matrix("Y1", y1.shape, y1)
     print("=============================== Method 2 ===============================")
-    # y = method2(h, x)
-    # print_matrix("Y", y.shape, y)
+    y2 = method2(y, 1.26)
+    print_matrix("Y2", y2.shape, y2)
